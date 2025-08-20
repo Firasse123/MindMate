9f2e5f6d00780ca1b4458349d7d66ef2fd3f0bac
Firasse123 <firasguizani2012@gmail.com>
2025-08-20 14:48:14 +0100

    chore: update server.js to use MongoDB connection

--- Backend/server.js
+++ Backend/server.js
@@ -3,11 +3,25 @@ const app=express();
 import dotenv from "dotenv";
 dotenv.config()
 
+import {connectDB,disconnectDB} from "./src/db/connection.js"
+
+app.use(express.json())
+
+
 app.get("/",(req,res)=>{
     res.send("Backend is running")
 })
 
 const PORT=process.env.PORT|| 5000;
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
+
+connectDB().then(()=>
+ {
+    app.listen(PORT,()=>{
+    console.log(`Server Open and Connected to the Database on port ${PORT}`)
 })
+ }
+)
+.catch((error)=>{
+    console.log(error)
+})
+
