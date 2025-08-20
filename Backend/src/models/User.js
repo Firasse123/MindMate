6dd557b59593b8cabda244f961b20018a9a82e12
Firasse123 <firasguizani2012@gmail.com>
2025-08-20 21:27:46 +0100

    Modele User crée

---
+++ Backend/src/models/User.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose"
+import {randomUUID} from "crypto"
+import { timeStamp } from "console"
+
+const UserSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    profile:{
+        firstName:String,
+        lastName:String,
+        age:Number,
+        avatar:String,
+
+    },
+    preferences: {
+      difficultyPreference: {
+        type: String,
+        enum: ['easy', 'medium', 'hard', 'adaptive'],
+        default: 'adaptive'
+      },
+      dailyGoal: {
+        type: Number,
+        default: 30 // minutes per day
+      }
+    },
+  stats: {
+    totalStudyTime:{type:Number,default:0},
+    totalQuizzesTaken:{type:Number,default:0},
+    averageScore:{type:Number,default:0},
+    lastStudyDate:{type:Date}
+
+},
+    timestamps:true
+})
+const userModel=mongoose.model("User",UserSchema)
+export default userModel
