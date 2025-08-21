6a3ddf87cfd6de1e411cc0ae5d70193376c0986e
Firasse123 <firasguizani2012@gmail.com>
2025-08-21 01:28:01 +0100

    added the validators

--- Backend/src/routes/user-routes.js
+++ Backend/src/routes/user-routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import {getAllUsers,userLogin,userRegister} from "../controllers/user-controller.js";
 const userRouter=express.Router();
-
+import {validate,signupValidator,loginValidator} from "../utils/validators.js";
 userRouter.get("/", getAllUsers);
-userRouter.post("/login", userLogin);
-userRouter.post("/register", userRegister);
+userRouter.post("/login", validate(loginValidator), userLogin);
+userRouter.post("/register", validate(signupValidator), userRegister);
 
 export default userRouter;
