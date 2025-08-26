6011f7f0dac8bbbbe1fcb7d818ea16fb92d3717b
Firasse123 <firasguizani2012@gmail.com>
2025-08-26 14:01:53 +0100

    Add the authentication routes

--- Backend/src/routes/user-routes.js
+++ Backend/src/routes/user-routes.js
@@ -1,9 +1,27 @@
 import express from "express";
-import {getAllUsers,userLogin,userRegister} from "../controllers/user-controller.js";
-const userRouter=express.Router();
-import {validate,signupValidator,loginValidator} from "../utils/validators.js";
-userRouter.get("/", getAllUsers);
-userRouter.post("/login", validate(loginValidator), userLogin);
-userRouter.post("/register", validate(signupValidator), userRegister);
+import {
+	login,
+	logout,
+	signup,
+	verifyEmail,
+	forgotPassword,
+	resetPassword,
+	checkAuth,
+} from "../controllers/auth.controller.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
+import { loginValidator,signupValidator } from "../utils/validators.js";	
 
-export default userRouter;
+const router = express.Router();
+
+router.get("/check-auth", verifyToken, checkAuth);
+
+router.post("/signup",signupValidator,loginValidator, signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+router.post("/verify-email", verifyEmail);
+router.post("/forgot-password", forgotPassword);
+
+router.post("/reset-password/:token", resetPassword);
+
+export default router;
